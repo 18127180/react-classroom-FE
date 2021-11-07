4e3c762d2cc8acd572a842ea4c5244dadd116812
Noobuster <nhanluu838@gmail.com>
2021-11-07 17:00:38 +0700

    router react

--- src/components/Authenticate/LoginForm.js
+++ src/components/Authenticate/LoginForm.js
@@ -1,45 +1,50 @@
-import React, { useState } from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import React, { useState } from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { GoogleLogin, GoogleLogout } from "react-google-login";
-
+import { Link as RouterLink } from "react-router-dom";
 
 function Copyright(props) {
   return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
       <Link color="inherit" href="https://mui.com/">
         Your Website
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
 
 const theme = createTheme();
 
-const CLIENT_ID = "780592097647-hif1svldddrkc4jpojqc44paile3l8da.apps.googleusercontent.com"
+const CLIENT_ID =
+  "780592097647-hif1svldddrkc4jpojqc44paile3l8da.apps.googleusercontent.com";
 
 const LoginForm = ({ section, topic, room, name }) => {
-
   const [state, setState] = useState({
     isLoggedIn: false,
     userInfo: {
       name: "",
-      emailId: ""
-    }
+      emailId: "",
+    },
   });
 
   const handleSubmit = (event) => {
@@ -47,14 +52,10 @@ const LoginForm = ({ section, topic, room, name }) => {
     const data = new FormData(event.currentTarget);
     // eslint-disable-next-line no-console
     console.log({
-      email: data.get('email'),
-      password: data.get('password'),
+      email: data.get("email"),
+      password: data.get("password"),
     });
   };
-
-  const responseGoogle = (response) => {
-    console.log(response);
-  }
 
   const responseGoogleSuccess = async (response) => {
     let userInfo = {
@@ -80,7 +81,7 @@ const LoginForm = ({ section, topic, room, name }) => {
 
   return (
     <ThemeProvider theme={theme}>
-      <Grid container component="main" sx={{ height: '100vh' }}>
+      <Grid container component="main" sx={{ height: "100vh" }}>
         <CssBaseline />
         <Grid
           item
@@ -88,12 +89,14 @@ const LoginForm = ({ section, topic, room, name }) => {
           sm={4}
           md={7}
           sx={{
-            backgroundImage: 'url(https://source.unsplash.com/random)',
-            backgroundRepeat: 'no-repeat',
+            backgroundImage: "url(https://source.unsplash.com/random)",
+            backgroundRepeat: "no-repeat",
             backgroundColor: (t) =>
-              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
+              t.palette.mode === "light"
+                ? t.palette.grey[50]
+                : t.palette.grey[900],
+            backgroundSize: "cover",
+            backgroundPosition: "center",
           }}
         />
         <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
@@ -101,18 +104,23 @@ const LoginForm = ({ section, topic, room, name }) => {
             sx={{
               my: 8,
               mx: 4,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
             }}
           >
-            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
               <LockOutlinedIcon />
             </Avatar>
             <Typography component="h1" variant="h5">
               Sign in
             </Typography>
-            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit}
+              sx={{ mt: 1 }}
+            >
               <TextField
                 margin="normal"
                 required
@@ -152,9 +160,11 @@ const LoginForm = ({ section, topic, room, name }) => {
                   </Link>
                 </Grid>
                 <Grid item>
-                  <Link href="#" variant="body2">
+                  {/* <RouterLink to="/register"> */}
+                  <Link component={RouterLink} to="/register" variant="body2">
                     {"Don't have an account? Sign Up"}
                   </Link>
+                  {/* </RouterLink> */}
                 </Grid>
               </Grid>
               {state.isLoggedIn ? (
@@ -184,6 +194,6 @@ const LoginForm = ({ section, topic, room, name }) => {
       </Grid>
     </ThemeProvider>
   );
-}
+};
 
 export default LoginForm;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import LoginForm from './components/Authenticate/LoginForm'
-import RegisterForm from './components/Authenticate/RegisterForm'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import LoginForm from "./components/Authenticate/LoginForm";
+import RegisterForm from "./components/Authenticate/RegisterForm";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-ReactDOM.render(<LoginForm/>,document.getElementById('root'));
-
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      <Route index path="login" element={<LoginForm />} />
+      <Route path="register" element={<RegisterForm />} />
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById("root")
+);

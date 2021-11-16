c63677278ea080a06fda9439aa4399a06a5e0f72
Noobuster <nhanluu838@gmail.com>
2021-11-16 11:56:29 +0700

    Update LoginForm.js

--- src/components/Authenticate/LoginForm.js
+++ src/components/Authenticate/LoginForm.js
@@ -194,7 +194,8 @@ const LoginForm = ({ section, topic, room, name }) => {
         <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
           <Box
             sx={{
-              my: 8,
+              mt: 6,
+              mb: 2,
               mx: 4,
               display: "flex",
               flexDirection: "column",

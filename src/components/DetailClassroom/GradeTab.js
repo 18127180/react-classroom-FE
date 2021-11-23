a6ffa27826b669f85bef3f404708cd81c8fb2d55
Le Hoang Phuc <phucyugi@gmail.com>
2021-11-24 00:10:50 +0700

    feat: integrate api get grade structure

--- src/components/DetailClassroom/GradeTab.js
+++ src/components/DetailClassroom/GradeTab.js
@@ -11,23 +11,56 @@ import Grid from "@mui/material/Grid";
 import DeleteIcon from "@mui/icons-material/Delete";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import FormControl from '@mui/material/FormControl';
+import Input from '@mui/material/Input';
+import InputLabel from '@mui/material/InputLabel';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import { Topic } from "@mui/icons-material";
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+import axios from "axios";
+import { useNavigate, useLocation } from "react-router-dom";
 
 const GradeTab = ({ data }) => {
-  const list = [
-    {
-      id: "1",
-      email: "test 1",
-    },
-    {
-      id: "2",
-      email: "test 2",
-    },
-    {
-      id: "3",
-      email: "test 3",
-    },
-  ];
-  const [characters, updateCharacters] = React.useState(list);
+  const [characters, updateCharacters] = React.useState([]);
+  const [topic, setTopic] = React.useState('Grade structure');
+  const [description, setDescription] = React.useState('Description');
+  const [loadEffect, setEffect] = React.useState(false);
+  const navigate = useNavigate();
+
+  const handleChangeTopic = (event) => {
+    setTopic(event.target.value)
+  }
+  const handleChangeDescription = (event) => {
+    setDescription(event.target.value)
+  }
+  const handleChangeGrade = (i, event) => {
+    const arr = [...characters];
+    arr[i].grade = Number(event.target.value);
+    updateCharacters(arr);
+  };
+
+  const handleChangeName = (i, event) => {
+    const arr = [...characters];
+    arr[i].name = event.target.value;
+    updateCharacters(arr);
+  };
+
+  const handleAddItem = () => {
+    let items = Array.from(characters);
+    items.push({
+      id: "" + (characters.length + 1),
+      name: "",
+      grade: 0
+    })
+    updateCharacters(items);
+    console.log(items);
+  }
+
+  const handleRemove = (id) => {
+    const newList = characters.filter((item) => item.id !== id);
+    updateCharacters(newList);
+  }
+
   const handleOnDragEnd = (result) => {
     if (!result.destination) return;
     const items = Array.from(characters);
@@ -37,7 +70,7 @@ const GradeTab = ({ data }) => {
   };
 
   const itemList = characters.map((item, index) => (
-    <Draggable key={item.id} draggableId={item.id} index={index}>
+    <Draggable key={""+item.id} draggableId={""+item.id} index={index}>
       {(provided) => (
         <Accordion
           {...provided.draggableProps}
@@ -45,7 +78,17 @@ const GradeTab = ({ data }) => {
           ref={provided.innerRef}
         >
           <AccordionSummary aria-controls="panel1a-content" id="panel1a-header">
-            <Typography>Accordion 1</Typography>
+            <FormControl variant="standard">
+              {/* <InputLabel htmlFor="component-simple">Name</InputLabel> */}
+              <Input id="component-simple" value={item.subject_name} onChange={e => handleChangeName(index, e)} />
+            </FormControl>
+            <FormControl variant="standard">
+              {/* <InputLabel htmlFor="component-simple">Name</InputLabel> */}
+              <Input id="component-simple" value={item.grade} onChange={e => handleChangeGrade(index, e)} />
+            </FormControl>
+            <IconButton aria-label="delete" onClick={() => handleRemove(item.id)}>
+              <DeleteIcon />
+            </IconButton>
           </AccordionSummary>
           <AccordionDetails>
             <Typography>
@@ -57,23 +100,87 @@ const GradeTab = ({ data }) => {
       )}
     </Draggable>
   ));
+
+  React.useEffect(() => {
+    const access_token = localStorage.getItem("access_token");
+    axios
+      .get(
+        process.env.REACT_APP_API_URL + `/classroom/grade-structure?class_id=${data.id}`,
+        {
+          headers: { Authorization: `Bearer ${access_token}` },
+        }
+      )
+      .then((res) => {
+        if (res.status === 200) {
+          updateCharacters(res.data?.list_syllabus);
+          setTopic(res.data?.topic);
+          setDescription(res.data?.description);
+          setEffect(true);
+        }
+      })
+      .catch((err) => {
+        if (err.response.status === 401) {
+          localStorage.removeItem("user");
+          localStorage.removeItem("access_token");
+          setEffect(false);
+          navigate("/login");
+        }
+      });
+  }, []);
+
   return (
-    <Box
-      sx={{
-        width: "100%",
-      }}
-    >
-      <DragDropContext onDragEnd={handleOnDragEnd}>
-        <Droppable droppableId="characters">
-          {(provided) => (
-            <Grid item xs={12} {...provided.droppableProps} ref={provided.innerRef}>
-              <List dense={true}>{itemList}</List>
-              {provided.placeholder}
-            </Grid>
-          )}
-        </Droppable>
-      </DragDropContext>
-    </Box>
+    <div>
+      {
+        loadEffect ? (
+          <Grid container direction="column" alignItems="center" justifyContent="space-between" >
+            <Box sx={{
+              width: "60%",
+              mt: 10
+            }}>
+              <FormControl variant="standard" fullWidth size="medium">
+                <Input id="component-simple" value={topic} onChange={handleChangeTopic} />
+              </FormControl>
+              <FormControl variant="standard" fullWidth>
+                <Input id="component-simple" value={description} onChange={handleChangeDescription} placeholder="Description" />
+              </FormControl>
+            </Box>
+            <Box
+              sx={{
+                width: "60%",
+                justifyContent: 'center',
+                display: 'flex'
+              }}
+            >
+              <DragDropContext onDragEnd={handleOnDragEnd}>
+                <Droppable droppableId="characters">
+                  {(provided) => (
+                    <Grid {...provided.droppableProps} ref={provided.innerRef}>
+                      <List dense={true}>{itemList}</List>
+                      {provided.placeholder}
+                    </Grid>
+                  )}
+                </Droppable>
+              </DragDropContext>
+            </Box>
+            <Box
+              sx={{
+                width: "60%",
+                justifyContent: 'center',
+                display: 'flex'
+              }}
+            >
+              <IconButton color="primary" aria-label="add to shopping cart" onClick={handleAddItem}>
+                <AddShoppingCartIcon />
+              </IconButton>
+              <IconButton color="primary" aria-label="add to shopping cart">
+                <CheckCircleOutlineIcon />
+              </IconButton>
+            </Box>
+          </Grid >
+        ) : (
+          <div></div>
+        )}
+    </div>
   );
 };
 

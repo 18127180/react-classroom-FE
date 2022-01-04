96a4345af6951d1ea047f9878282193c1b08becf
Thien Nhan <nhanluu838@gmail.com>
2022-01-04 21:26:17 +0700

    ui cho admin show classes

--- src/components/Admin/ClassTable.js
+++ src/components/Admin/ClassTable.js
@@ -18,6 +18,9 @@ import SaveAlt from "@mui/icons-material/SaveAlt";
 import Search from "@mui/icons-material/Search";
 import ViewColumn from "@mui/icons-material/ViewColumn";
 import SaveIcon from "@mui/icons-material/Save";
+import { Avatar, Chip, Grid, Stack, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import { Fragment } from "react";
 
 const tableIcons = {
   Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
@@ -41,19 +44,56 @@ const tableIcons = {
 
 const data = [
   {
-    id: "1",
+    id: 1,
     name: "Web programming",
     section: "Web",
+    teachers: [
+      {
+        name: "Narui",
+        avatar:
+          "https://scontent-sin6-1.xx.fbcdn.net/v/t1.6435-1/p100x100/97291515_2722794567830001_9103251667125534720_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=7206a8&_nc_ohc=v8cE8PXj8PMAX8rIu6z&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent-sin6-1.xx&oh=00_AT8mrP9GcmuQsglOtUbR-cdJ7qxv9OV8smvBsZ-mEtEmEw&oe=61F89ACF",
+      },
+      {
+        name: "Phuc map",
+        avatar:
+          "https://scontent-sin6-1.xx.fbcdn.net/v/t1.6435-1/p100x100/97291515_2722794567830001_9103251667125534720_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=7206a8&_nc_ohc=v8cE8PXj8PMAX8rIu6z&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent-sin6-1.xx&oh=00_AT8mrP9GcmuQsglOtUbR-cdJ7qxv9OV8smvBsZ-mEtEmEw&oe=61F89ACF",
+      },
+    ],
+    students: [
+      {
+        name: "Son Map",
+        avatar:
+          "https://scontent-sin6-1.xx.fbcdn.net/v/t1.6435-1/p100x100/97291515_2722794567830001_9103251667125534720_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=7206a8&_nc_ohc=v8cE8PXj8PMAX8rIu6z&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent-sin6-1.xx&oh=00_AT8mrP9GcmuQsglOtUbR-cdJ7qxv9OV8smvBsZ-mEtEmEw&oe=61F89ACF",
+      },
+      {
+        name: "Thanh Oni",
+        avatar:
+          "https://scontent-sin6-1.xx.fbcdn.net/v/t1.6435-1/p100x100/97291515_2722794567830001_9103251667125534720_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=7206a8&_nc_ohc=v8cE8PXj8PMAX8rIu6z&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent-sin6-1.xx&oh=00_AT8mrP9GcmuQsglOtUbR-cdJ7qxv9OV8smvBsZ-mEtEmEw&oe=61F89ACF",
+      },
+    ],
+    grade_structure: [
+      {
+        name: "Midterm",
+        max: 100,
+      },
+      {
+        name: "Final",
+        max: 456,
+      },
+    ],
+    topic: "Bla bla bla",
   },
   {
-    id: "2",
+    id: 2,
     name: "NFT",
     section: "Blockchain",
+    topic: "Bla bla bla",
   },
   {
     id: 3,
     name: "Automation Testing",
     section: "Test",
+    topic: "Bla bla bla",
   },
 ];
 
@@ -66,18 +106,87 @@ const ClassTable = () => {
           { title: "ID", field: "id" },
           { title: "Name", field: "name" },
           { title: "Section", field: "section" },
+          { title: "Topic", field: "topic" },
         ]}
         data={data}
         title="Classrooms"
-        actions={[
-          {
-            icon: () => <SaveIcon color="primary" />,
-            tooltip: "Save User",
-            onClick: (_event, rowData) => alert("You saved user " + rowData.id),
-          },
-        ]}
-        options={{
-          actionsColumnIndex: -1,
+        onRowClick={(event, rowData, togglePanel) => togglePanel()}
+        detailPanel={(rowData) => {
+          return (
+            <section style={{ padding: "16px" }}>
+              <Grid container direction="row">
+                <Grid item xs={8}>
+                  <Grid container direction="column" spacing={1}>
+                    {rowData.teachers?.length > 0 && (
+                      <Fragment>
+                        <Grid item>
+                          <Typography variant="h6">Teachers</Typography>
+                        </Grid>
+                        <Grid item>
+                          <Stack direction="row" spacing={1.5}>
+                            {rowData.teachers.map((teacher) => (
+                              <Chip
+                                avatar={<Avatar alt={teacher.name} src={teacher.avatar} />}
+                                label={teacher.name}
+                                size="medium"
+                                variant="outlined"
+                                sx={{ backgroundColor: "#e04646", color: "white" }}
+                              />
+                            ))}
+                          </Stack>
+                        </Grid>
+                      </Fragment>
+                    )}
+                    {rowData.students?.length > 0 && (
+                      <Fragment>
+                        <Grid item>
+                          <Typography variant="h6">Students</Typography>
+                        </Grid>
+                        <Grid item>
+                          <Stack direction="row" spacing={1.5}>
+                            {rowData.students.map((student) => (
+                              <Chip
+                                avatar={<Avatar alt={student.name} src={student.avatar} />}
+                                label={student.name}
+                                size="medium"
+                                variant="outlined"
+                                sx={{ backgroundColor: "#cdc6c6", color: "black" }}
+                              />
+                            ))}
+                          </Stack>
+                        </Grid>
+                      </Fragment>
+                    )}
+                  </Grid>
+                </Grid>
+                <Grid item xs={4}>
+                  <Grid container direction="column" spacing={1.5}>
+                    {rowData.grade_structure && (
+                      <Fragment>
+                        <Grid item>
+                          <Typography variant="h5">Grade Structure</Typography>
+                        </Grid>
+                        <Grid item>
+                          <table class="grade-structure">
+                            <tr>
+                              <th>Name</th>
+                              <th>Max point</th>
+                            </tr>
+                            {rowData.grade_structure.map((grade) => (
+                              <tr data-ng-repeat="customer in people | filter: table">
+                                <td>{grade.name}</td>
+                                <td>{grade.max}</td>
+                              </tr>
+                            ))}
+                          </table>
+                        </Grid>
+                      </Fragment>
+                    )}
+                  </Grid>
+                </Grid>
+              </Grid>
+            </section>
+          );
         }}
       />
     </div>
